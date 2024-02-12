5d42cb1fd7ea33f585e0b61e16a3e733f07e13ce
AvgmMrcl <b.j.komarnicki@protonmail.com>
2024-02-12 11:03:48 +0100

    Initial Commit

---
+++ amplify/amplifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  amplifyApp.swift
+//  amplify
+//
+//  Created by Bartłomiej Komarnicki on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct amplifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
